5dd2af8d93447529e19aa954a5051f186b09714a
JhonLlapapasca <96848938+JhonLlapapasca@users.noreply.github.com>
2024-04-20 17:38:02 -0500

    Add files via upload

---
+++ src/main/java/com/mycompany/sgvp/rimdeca/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.sgvp.rimdeca;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
